64d04ccfd698871e7f95d55f4a2f6105f17118ef
Lisandro H G Mosconi Fariña <lisandro.mosconi@gmail.com>
2026-05-15 17:21:32 +0000

    feat: added connection with UserGameList and UserGLG

--- src/api/api_routes/favorites.py
+++ src/api/api_routes/favorites.py
@@ -1,75 +1,50 @@
-from flask import jsonify
-from api.models import db, Favorite, Game
+from flask import jsonify, request
+from api.models import db, Game, UserGameList, UserGLG
 from api.routes import api
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from sqlalchemy import select
 
 # GET /favorites
-@api.route('/favorites', methods=['GET'])
+@api.route('/favorite/change', methods=['PUT'])
 @jwt_required()
-def get_favorites():
-
-    current_user_id = get_jwt_identity() #Mirar si hay que eliminar
-
-    favorites = db.session.execute(
-        select(Favorite).where(Favorite.user_id == current_user_id)
-    ).scalars().all()
-
-    return jsonify([f.serialize() for f in favorites]), 200
-
-# POST /favorites/<game_id>
-@api.route('/favorites/<int:game_id>', methods=['POST'])
-@jwt_required()
-def add_favorite(game_id):
+def change_favorite():
 
     current_user_id = get_jwt_identity()
 
-    game = db.session.get(Game, game_id)
-
+    body= request.get_json()
+    game_id= body.get("game_id")
+    if not game_id: 
+        return jsonify({"msg": "game_id is a required field", "success": False}), 400
+    
+    game= db.session.get(Game, game_id).scalar_one_or_none()
     if not game:
-        return jsonify({"msg": "Game not found"}), 404
+        return jsonify({"msg": "game not found"}), 404
     
-    existing = db.session.execute(
-        select(Favorite).where(
-            Favorite.user_id == current_user_id,
-            Favorite.game_id == game_id
-        )
-    ).scalar_one_or_none()
+    user_game_list = db.session.execute(select(UserGameList).where(
+        UserGameList.user_id == current_user_id
+    )).scalar_one_or_none()
 
-    if existing:
-        return jsonify({"msg": "Already in favorites"}), 400
+    if not user_game_list:
+        return jsonify({"msg": "User game list not found", "success": False}), 404
+
+    user_glg = db.session.execute(select(UserGLG).where(
+        UserGLG.ugl_id == user_game_list.id,
+        UserGLG.game_id == game_id
+    )).scalar_one_or_none()
+
+    if not user_glg:
+        return jsonify({"msg": "Game is not in user list", "success": False}), 404
     
-    favorite = Favorite(
-        user_id=current_user_id,
-        game_id=game_id
-    )
-
-    db.session.add(favorite)
+    user_glg.is_favorite = not user_glg.is_favorite
     db.session.commit()
 
     return jsonify({
-        "msg": "Favorite added",
-        "favorite": favorite.serialize()
-    }), 201
+        "msg": "Favorite updated", "success": True, "is_favorite": user_glg.is_favorite
+    }), 200
 
-# DELETE /favorites/<game_id>
-@api.route('favorites/<int:game_id>', methods=['DELETE'])
-@jwt_required()
-def remve_favorite(game_id):
 
-    current_user_id = get_jwt_identity()
-
-    favorite = db.session.execute(
-        select(Favorite).where(
-            Favorite.user_id == current_user_id,
-            Favorite.game_id == game_id
-        )
-    ).scalar_one_or_none()
-
-    if not favorite:
-        return jsonify({"msg": "Favorite not found"}), 404
-    
-    db.session.delete(favorite)
-    db.session.commit()
-
-    return jsonify({"msg": "Favorite removed"}), 200
+    #buscar UserGameList que tenga el id del usuario
+    #una vez encontrado, crear una variable usergl_id, la cual va aguardar el id del UserGameList encontrado
+    #busco UserGLG que su usergl_id sea el mismo que el de la variable creada anteriormente Y que su game_id sea el mismo que el de la variable game_id
+    #una vez encontrado UserGLG, le cambio la propiedad is_favorite a lo opuesto de lo que era antes !
+    #guardar los cambios y return jsonify
